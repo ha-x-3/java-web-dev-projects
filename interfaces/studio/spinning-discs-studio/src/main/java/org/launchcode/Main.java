d56e1a73ab2c2dd337838045d975aaf5ddd11946
Brittney Epperson <beppers87@hotmail.com>
2023-09-25 19:59:11 -0500

    Completed studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        CD cd = new CD("CD Example", 700, "CD-R", 350);
+        CD cd = new CD("Super Cool CD", 700, "CD-R", 350);
         DVD dvd = new DVD("DVD Example", 4700, "DVD-R", 1450);
 
         cd.spinDisc();
